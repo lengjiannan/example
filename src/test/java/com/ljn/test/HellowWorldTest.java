dd4849af6685c6aedbfc15a123dcb2a179d61c77
lengjiannan <lengjiannan1@icloud.com>
2018-10-31 22:59:17 +0800

    1

---
+++ src/test/java/com/ljn/test/HellowWorldTest.java
@@ -0,0 +1,35 @@
+package com.ljn.test; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* HellowWorld Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>十月 31, 2018</pre> 
+* @version 1.0 
+*/ 
+public class HellowWorldTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
